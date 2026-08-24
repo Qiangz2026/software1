a71955336b9d9ebef2fe19d39f33cfc33a9f6e58
Qiangz2026 <qiang.ada.zhang@gmail.com>
2026-08-24 22:29:16 +0300

    Exercise1 in Module5

---
+++ Module5/exercise1.py
@@ -0,0 +1,13 @@
+number = 1
+while number <= 1000:
+    if number%3 == 0:
+        print(str(number))
+    number = number + 1
+
+
+# number = 1
+# while number <= 1000:
+#     while number%3 == 0:
+#         print(str(number))
+#         number = number + 1
+#     number = number + 1
