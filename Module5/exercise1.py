cc5f0c44aad7a2d789a3517d77426a44b9c3cdc9
Qiangz2026 <qiang.ada.zhang@gmail.com>
2026-08-26 10:20:24 +0300

    changed some code

--- Module5/exercise1.py
+++ Module5/exercise1.py
@@ -1,13 +1,6 @@
 number = 1
 while number <= 1000:
     if number%3 == 0:
-        print(str(number))
+        print(number)
+#In this condition, no need to convert number to string, because print() do automatic conversion. Only we do some character concatenation output.
     number = number + 1
-
-
-# number = 1
-# while number <= 1000:
-#     while number%3 == 0:
-#         print(str(number))
-#         number = number + 1
-#     number = number + 1
