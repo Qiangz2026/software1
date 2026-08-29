f32cb51c8316b3f7cf2d78b9d8983b3df4febe83
Qiangz2026 <qiang.ada.zhang@gmail.com>
2026-08-29 21:18:26 +0300

    player's information

--- project/player's information.py
+++ project/player's information.py
@@ -1,4 +1,5 @@
-print("Welcome to My Day")
+game_name = "My Day!"
+print(f"Welcome to {game_name}")
 name  = input("Hi, enter your name here: ")
 age = input("Please also enter your age: ")
 print(name)
