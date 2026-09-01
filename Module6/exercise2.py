a5f10fc742d407cb3d76b9d922e90fe40cc00a90
Qiangz2026 <qiang.ada.zhang@gmail.com>
2026-09-01 16:10:26 +0300

    modify code exercise2 of module6

--- Module6/exercise2.py
+++ Module6/exercise2.py
@@ -4,8 +4,9 @@ while number != "":
     number = float(number)
     numbers.append(number)
     number = input("Enter a number: ")
-numbers.sort(reverse=True)   #Sort the elements in the list from largest to smallest.
-# numbers.sort() and numbers.reverse()  the same function with previous line
+# numbers.sort(reverse=True)   #Sort the elements in the list from largest to smallest.
+numbers.sort()
+numbers.reverse()
 print("The greatest numbers in descending order: ")
 if len(numbers) >= 5: #If the list contains five or more elements, Output the first five
     for n in numbers[0:5]:
