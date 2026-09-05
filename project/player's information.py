4cab930679250e484294af1a77b103c15f33f540
Qiangz2026 <qiang.ada.zhang@gmail.com>
2026-09-05 21:25:37 +0300

    change file name

--- project/player's information.py
+++
@@ -1,6 +0,0 @@
-game_name = "Finnish language challenge!"
-print(f"Welcome to {game_name}")
-player_name  = input("Hi, enter your name here: ")
-player_age = input("Please also enter your age: ")
-print(player_name)
-print(player_age)
