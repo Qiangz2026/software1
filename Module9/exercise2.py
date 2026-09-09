1e4bbf663cc9fefb996cc201e4e17b8082f03822
Qiangz2026 <qiang.ada.zhang@gmail.com>
2026-09-09 10:21:47 +0300

    modify code exercise2 in module9

--- Module9/exercise2.py
+++ Module9/exercise2.py
@@ -1,9 +1,9 @@
 class Car:
-    def __init__(self, license_plate, maximum_speed, current_speed = 0, travelled_distance = 0):
+    def __init__(self, license_plate, maximum_speed):
         self.license_plate = license_plate
         self.maximum_speed = maximum_speed
-        self.current_speed = current_speed
-        self.travelled_distance = travelled_distance
+        self.current_speed = 0
+        self.travelled_distance = 0
         
     def accelerate(self, speed_change):
         self.current_speed += speed_change
